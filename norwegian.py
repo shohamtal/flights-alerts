db8acd6ff35412a82c63576f0ecc575e70487c9d
Shoham Tal <shoham.tal@signalsgroup.com>
2017-03-12 15:39:33 +0200

    f

--- norwegian.py
+++ norwegian.py
@@ -58,3 +58,62 @@ class NorwegianScrapper(FlightScrapper):
                         fly_back_rates.append(int(float(lbl.text)))
 
         return fly_to_rates, fly_back_rates, url
+
+
+class KiwiScrapper(FlightScrapper):
+    def __init__(self):
+        raise NotImplementedError
+        pass
+
+    def _day(self, dt):
+        return datetime.strftime(dt, "%d")
+
+    def _monthyear(self, dt):
+        return datetime.strftime(dt, "%Y%m")
+
+
+    def get_rates(self, fly_out_date, fly_back_date, fly_from, fly_to):
+        # dates sample 2017/03/26
+        # fly_from sample 'TLV'
+
+        d_out = self._day(fly_out_date)
+        moye_out = self._monthyear(fly_out_date)
+        d_back = self._day(fly_back_date)
+        moye_back = self._monthyear(fly_back_date)
+        currency = 'EUR'
+
+        url = 'https://www.kiwi.com/il/search/me%E1%BA%96oz-tel-aviv-medinat-yisra%E2%80%99el-250km/london-united-kingdom/2017-04-16/2017-04-20?daysInWeek=0-4'
+
+        url2 = 'https://www.norwegian.com/en/booking/flight-tickets/select-flight/?A_City={fly_to}&AdultCount=1' \
+              '&ChildCount=0&CurrencyCode={currency}&D_City={fly_from}&D_Day={d_out}&D_Month={moye_out}&IncludeTransit=true' \
+              '&InfantCount=0&R_Day={d_back}&R_Month={moye_back}&TripType=2'.format(
+            currency=currency, d_out=d_out, moye_out=moye_out, d_back=d_back, moye_back=moye_back,
+            fly_from=fly_from, fly_to=fly_to)
+        res = requests.get(url)
+        if res.status_code >= 400:
+            res.raise_for_status()
+
+        soup = BeautifulSoup(res.content)
+        tables = soup.findAll("div", {"class": "Sorting-content"})
+
+        fly_to_rates = []
+        trs = tables[0].find('tbody').findAll('tr')
+        for tr in trs:
+            if 'rowinfo1' in tr['class']:
+                tds = tr.findAll('td')
+                for td in tds:
+                    if 'fareselect' in td['class']:
+                        lbl = td.find('div').find('label')
+                        fly_to_rates.append(int(float(lbl.text)))
+
+        fly_back_rates = []
+        trs = tables[1].find('tbody').findAll('tr')
+        for tr in trs:
+            if 'rowinfo1' in tr['class']:
+                tds = tr.findAll('td')
+                for td in tds:
+                    if 'fareselect' in td['class']:
+                        lbl = td.find('div').find('label')
+                        fly_back_rates.append(int(float(lbl.text)))
+
+        return fly_to_rates, fly_back_rates, url
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-import datetime
+from datetime import timedelta
 
-from norwegian import NorwegianScrapper
+from norwegian import *
 from slacker import Slacker
 
 
@@ -13,24 +13,22 @@ def next_weekday(startdate, weekday):
     @startdate: given date
     @weekday: week day as a integer, between 0 (Monday) to 6 (Sunday)
     """
-    t = datetime.timedelta((7 + weekday - startdate.weekday()) % 7)
+    t = timedelta((7 + weekday - startdate.weekday()) % 7)
     return (startdate + t)
 
 
 
 days_delta = 4
-today = datetime.date.today()
-two_days_from_now = today + datetime.timedelta(days=2)
+today = datetime.today().date()
+two_days_from_now = today + timedelta(days=2)
 sunday1 = next_weekday(two_days_from_now, 6)
 thursday1 = next_weekday(sunday1, 3)
-sunday2 = next_weekday(sunday1+ datetime.timedelta(days=1), 6)
-thursday2 = next_weekday(sunday2, 3)
-sunday3 = next_weekday(sunday2+ datetime.timedelta(days=1), 6)
-thursday3 = next_weekday(sunday3, 3)
-sunday4 = next_weekday(sunday3+ datetime.timedelta(days=1), 6)
-thursday4 = next_weekday(sunday4, 3)
-
-dates_list = [(sunday1, thursday1), (sunday2, thursday2), (sunday3, thursday3), (sunday4, thursday4)]
+dates_list = [(sunday1, thursday1)]
+for x in range(0, 14):
+    prev_sun = dates_list[x][0]
+    sun = next_weekday(prev_sun + timedelta(days=1), 6)
+    thu = next_weekday(sun, 3)
+    dates_list.append((sun, thu))
 
 
 if __name__ == '__main__':
